cfaffb2554b55c6b615a5486d18fb03c2e90aa1a
ALI_Zucker <zabihiali1998@gmail.com>
2023-12-26 02:04:19 +0330

    one day

---
+++ assets/js/script.js
@@ -0,0 +1,112 @@
+const imag = document.getElementById('imag_main');
+const imag_in = document.getElementById('imge-side');
+
+const UserLogin = document.getElementById('userName');
+const PassLogin = document.getElementById('passwordlogin')
+
+const UserRegist = document.getElementById('userName2');
+const EmailRegist = document.getElementById('userName3');
+const PassRegist = document.getElementById('passwordlogin2');
+const error = document.getElementById('error');
+const error1 = document.getElementById('error1');
+const error2 = document.getElementById('error2');
+const showme = document.getElementById('show_me');
+const showmetext = document.getElementById('showtextme');
+
+//---------------------------function----------------
+function create(param) {
+    if (param == 1) {
+        let textUsername = UserLogin.value;
+        let passlogin = PassLogin.value;
+
+        const person = {Username: textUsername, Password: passlogin}
+        show("Username:" + person["Username"] + "----Password:" + person["Password"], 1)
+
+
+    } else if (param == 2) {
+        let textUsername = UserRegist.value;
+        let emailusername = EmailRegist.value
+        let passlogin = PassRegist.value;
+
+        const person = {Username: textUsername, EmailUser: emailusername, Password: passlogin}
+        show("Username: " + person["Username"] + "----Password: " + person["Password"] + "-----Email: " + person["EmailUser"], 1)
+
+    }
+}
+
+
+//---------------------------function style----------
+function SwapRight() {
+    imag.style.transform = "translateX(42vw)";
+    imag_in.style.borderBottomRightRadius = "0";
+    imag_in.style.borderBottomLeftRadius = "400px";
+    imag_in.style.transition = "all 1.4s linear";
+    imag.style.transition = "all 1.4s  ease-in-out";
+}
+
+function Swapleft() {
+    imag.style.transform = "translateX(0)";
+    imag_in.style.borderBottomRightRadius = "300px";
+    imag_in.style.borderBottomLeftRadius = "0";
+    imag_in.style.transition = "all 1.4s linear";
+    imag.style.transition = "all 1.4s  ease-in-out";
+}
+
+function changetext(part) {
+    if (part == 1) {
+        if (/^[A-Za-z0-9]*$/.test(UserLogin.value)) {
+            error.innerText = "Ok.your username is currently ";
+            error.style.display = "inline-block"
+            error.style.color = "green"
+            UserLogin.style.animation = "none"
+        } else {
+            error.innerText = "string contains only letters and numbers. ";
+            error.style.display = "inline-block"
+            error.style.color = "red"
+            UserLogin.style.animation = "tilt-shaking";
+            UserLogin.style.animationDuration = "0.3s";
+            UserLogin.style.animationIterationCount = "infinite";
+            UserLogin.style.animationTimingFunction = "linear";
+        }
+
+    } else if (part == 2) {
+        if (/^[A-Za-z0-9]*$/.test(UserRegist.value)) {
+            error1.innerText = "Ok.your username is currently ";
+            error1.style.display = "inline-block"
+            error1.style.color = "green"
+            UserRegist.style.animation = "none"
+        } else {
+            error1.innerText = "string contains only letters and numbers. ";
+            error1.style.display = "inline-block"
+            error1.style.color = "red"
+            UserRegist.style.animation = "tilt-shaking";
+            UserRegist.style.animationDuration = "0.3s";
+            UserRegist.style.animationIterationCount = "infinite";
+            UserRegist.style.animationTimingFunction = "linear";
+        }
+    } else if (part == 3) {
+        if (EmailRegist.value.includes("@gmail.com")) {
+            error2.innerText = "Ok.your username is currently ";
+            error2.style.display = "inline-block"
+            error2.style.color = "green"
+
+        } else {
+            error2.innerText = "string contains only letters and numbers. ";
+            error2.style.display = "inline-block"
+            error2.style.color = "red"
+
+        }
+    }
+}
+
+function show(str, select) {
+    if (select == 1) {
+        showme.style.display = "block";
+        showmetext.innerText = str;
+    } else {
+        showme.style.display = "none";
+    }
+
+}
+
+
